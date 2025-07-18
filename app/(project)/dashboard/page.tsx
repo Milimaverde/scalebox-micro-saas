1c91f9bd4a20d04978e843caf6e9204e00f78759
Millena Limaverde <millenalimaverde@outlook.com>
2025-07-18 19:51:16 -0300

    feat(ui): enhance Dashboard and Payments pages with new header layout and styled buttons

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { handleAuth } from "@/app/actions/handle-auth";
  import { auth } from '@/app/lib/auth';
 import Link from "next/link";
  import { redirect } from 'next/navigation';
+import { Button } from "../components/buttons";
  
  export default async function Dashboard() {
    const session = await auth();
@@ -11,18 +12,34 @@ import Link from "next/link";
    }
  
    return (
-     <div className="flex flex-col gap-10 items-center justify-center h-screen">
-       <h1 className="text-4xl font-bold">Protected Dashboard</h1>
-       <p>{session?.user?.email ? session?.user?.email : 'Usuário não está logado!'}</p>
+<>
+    <header className="flex py-5 justify-between">
+      <Link href="/">
+        <h3 className="text-2xl font-semibold text-white">ScaleBox</h3>
+      </Link>
+      <div>
        {session.user?.email && (
          <form action={handleAuth}>
-           <button type="submit" className="border rounded-md px-2 py-1 cursor-pointer">
+           <button className="rounded-2xl px-6 py-3 text-lg cursor-pointer transition-all duration-150 bg-buttonyellow hover:bg-buttonyellow-hover">
              Logout
            </button>
          </form>
        )}
-
-       <Link href="/payments"> Pagamentos </Link>
      </div>
+      </header>
+    <section className="my-16 flex flex-col justify-center md:max-w-5xl md:mx-auto items-center">
+          <h1 className="font-semibold text-center text-4xl lg:text-6xl leading-normal text-white mb-6 ">
+             Protected Dashboard
+          </h1>
+          <p className="text-lg text-center leading-8 text-white/40">
+              ScaleBox, aqui você está logado com o Google e pode efetuar pagamentos com Stripe e Mercado Pago clicando no botão 'Pagamentos' ou deslogar clicando em 'Logout'.
+          </p>
+       <Link href="/payments">
+       <Button className="mt-10 py-3 px-6" color='purple' hoverColor='hoverpurple'>Pagamentos</Button>
+       </Link>
+    </section>
+    
+     </>
    );
+    
  }
--- app/(project)/payments/page.tsx
+++ app/(project)/payments/page.tsx
@@ -2,6 +2,8 @@
  
 import { useMercadoPago } from '@/app/hooks/useMercadoPago';
 import { useStripe } from '@/app/hooks/useStripe';
+import { Button } from '../components/buttons';
+import Link from 'next/link';
  
  export default function Payments() {
    const {
@@ -15,28 +17,34 @@ import { useStripe } from '@/app/hooks/useStripe';
    } = useMercadoPago();
  
    return (
-     <div className="flex flex-col gap-10 items-center justify-center h-screen">
-       <h1 className="text-4xl font-bold">Pagamentos</h1>
-       <button className="border rounded-md px-1" onClick={() => createPaymentStripeCheckout({ testeId: '123' })}>
-         Criar Pagamento Stripe
-       </button>
-       <button
-         className="border rounded-md px-1"
-         onClick={() => createSubscriptionStripeCheckout({ testeId: '123' })}
-       >
-         Criar Assinatura Stripe
-       </button>
-       <button className="border rounded-md px-1" onClick={() => handleCreateStripePortal()}>
-         Criar Portal de Pagamento
-       </button>
-       <button
-         className="border rounded-md px-1"
-         onClick={() => createMercadoPagoCheckout({
-            testeId: '123',
-            userEmail: 'teste@email.com'
-         })}>
-         Criar Pagamento Mercado Pago
-       </button>
-     </div>
+    <>
+      <header className="flex py-5 justify-between">
+        <Link href="/">
+        <h3 className="text-2xl font-semibold text-white">ScaleBox</h3>
+        </Link>
+      </header>
+      <section className="my-16 flex flex-col justify-center md:max-w-5xl md:mx-auto items-center">
+        <h1 className="font-semibold text-center text-4xl lg:text-6xl leading-normal text-white mb-6 ">
+             Pagamentos
+             </h1>
+             <p className="text-lg text-center leading-8 text-white/40">
+              Nessa tela é feita uam simulação de um produto X, você pode efetuar pagamentos com Stripe e Mercado Pago, além de adquirir assinaturas e acessar o portal de pagamentos do Stripe.
+              </p>
+              <div className="items-center mt-10 flex flex-col gap-4">
+
+              <button className="rounded-2xl px-6 py-3 text-lg cursor-pointer transition-all duration-150 bg-buttonyellow hover:bg-buttonyellow-hover" onClick={() => createPaymentStripeCheckout({ testeId: '123' })}>Pagamento com Stripe
+              </button>
+              <button className="rounded-2xl px-6 py-3 text-lg cursor-pointer transition-all duration-150 bg-buttonyellow hover:bg-buttonyellow-hover" onClick={() => createSubscriptionStripeCheckout({ testeId: '123' })}> Assinatura com Stripe
+              </button>
+              <div className='hidden'>
+                <button className="rounded-2xl px-6 py-3 text-lg cursor-pointer transition-all duration-150 bg-buttonyellow hover:bg-buttonyellow-hover" onClick={() => handleCreateStripePortal()}>Efetuar Portal de Pagamento</button>
+                </div>
+                <button className="rounded-2xl px-6 py-3 text-lg cursor-pointer transition-all duration-150 bg-buttonyellow hover:bg-buttonyellow-hover" onClick={() => createMercadoPagoCheckout({ testeId: '123',userEmail: 'teste@email.com'
+                })}>Pagamento com Mercado Pago</button>
+              </div>
+                </section>
+              
+
+    </>
    );
  }
